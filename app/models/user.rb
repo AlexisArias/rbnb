d7eb468d2fc7270344f7d6784904e0ca2c349536
Baptiste Garrigues <garrigues.baptiste@gmail.com>
2024-02-26 15:53:37 +0100

    Initialisation finale

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :rentals
-  validates :first_name, :last_name, :email, :profile_picture, presence: true
+  validates :first_name, :last_name, :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
