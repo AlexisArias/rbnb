0c904b94e867715087d9c2947208fb8bb57f100d
Baptiste Garrigues <garrigues.baptiste@gmail.com>
2024-02-28 14:38:34 +0100

    Modifications du format de la date

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
+
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr.localize(flatpickr.l10ns.fr)
+    flatpickr(this.element, {
+      altInput: true,
+      dateFormat: "Y-m-d",
+    })
+
   }
 }
