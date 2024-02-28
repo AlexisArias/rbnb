fd7164cd5255e316803772c97c7612e68288188d
Baptiste Garrigues <garrigues.baptiste@gmail.com>
2024-02-28 14:54:53 +0100

    Last

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
   connect() {
     flatpickr(this.element, {
       altInput: true,
-      altFormat: "F j, Y",
+      altFormat: "j F Y",
       dateFormat: "Y-m-d",
       locale: "fr",
     })
